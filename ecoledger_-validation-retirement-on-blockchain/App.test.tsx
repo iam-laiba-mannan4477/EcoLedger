8ef092fbfdd360792201fe20164716e79168fb8e
laiba mannan <laibamannan73@gmail.com>
2025-12-16 22:10:08 +0500

    Add initial test for Example component

---
+++ ecoledger_-validation-retirement-on-blockchain/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import React from 'react'
+
+function Example() {
+  return <div>Hello, Vitest!</div>
+}
+
+test('renders greeting text', () => {
+  render(<Example />)
+  expect(screen.getByText('Hello, Vitest!')).toBeInTheDocument()
+})
